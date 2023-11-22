6d01ea6303283431a1896659a510d1f88ceaeb38
João Praia <joaopraia@joaopraia.com.br>
2023-11-21 21:20:17 -0300

    Identação do arquivo BasicController

--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +20,5 @@ public class BasicController {
 	public List<Persons> getAll() {
 		
 		return personService.findAll();
-	}
-	
-	
-	
+	}	
 }
